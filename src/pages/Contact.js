b7dea0f596b8d57f4415dfebb521429e3eeedc36
esam <esam_95@hotmail.se>
2023-09-27 21:29:19 +0200

    Contact content done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,32 +4,19 @@ export default function Contact() {
   return (
     <div>
       <h3>Om du är intresserad av att kontakta mig för arbetsfrågor eller annat, tveka inte att nå ut till mig:</h3>
-      <div style={styles.contactItem}>
-        <i class="fas fa-linkedin"></i>
-        <a href="länk-till-Linkeind">LinkedIn</a>
-      </div>
-      <div style={styles.contactItem}>
-        <i class="fab fa-github"></i>
-        <a href="länk-till-Github">GitHub</a>
-      </div>
-      <div style={styles.contactItem}>
-        <i class="fas fa-envelope"></i>
-        <a href="mailto:din-email@example.com">Mail</a>
-      </div>
-      <div style={styles.contactItem}>
-        <i class="fas fa-phone"></i>
-        <a href="tel:telefonnummer">Telefon</a>
-      </div>
+      <a href="https://www.linkedin.com/in/esam-ali-gelani-181755177/" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-linkedin-in" style={{ color: '#0077B5', fontSize: '3em' }}></i>
+      </a>
+      <a href="https://github.com/esam95" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-github" style={{ color: '#211F1F', fontSize: '3em' }}></i>
+      </a>
+      <a href="mailto:esam_95@hotmail.se" target="_blank" rel="noopener noreferrer">
+        <i className="fas fa-envelope" style={{ color: '#D44638', fontSize: '3em' }}></i>
+      </a>
+      <a href="tel:+46729214790" target="_blank" rel="noopener noreferrer">
+        <i className="fas fa-phone" style={{ color: '#075E54', fontSize: '3em' }}></i>
+      </a>
     </div>
 
   )
 }
-
-const styles = {
-    contactItem: {
-      display: 'flex',
-      alignItems: 'center',
-      marginBottom: '20px',
-    }
-  };
-  
